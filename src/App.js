65d93c12265d6fd072a33ad7a964d328944e672b
Johnny <idsopy@nate.com>
2022-01-11 16:39:02 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import logo from "./logo.svg";
 import "./App.css";
 import {
@@ -8,80 +9,83 @@ import {
   Container,
   Jumbotron,
 } from "react-bootstrap";
-import { useState } from "react";
+import React, { useState } from "react";
+import Data from "./data.js";
 
 function App() {
-  let [shoes, shoes변경] = useState([]);
+  let [shoes, shoes변경] = useState(Data);
+
   return (
     <div className="App">
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-
-      <div class="jumbotron background">
-        <h1 class="display-4">20% Season Off</h1>
-        <p class="lead">
-          This is a simple hero unit, a simple jumbotron-style component for
-          calling extra attention to featured content or information.
-        </p>
-
-        <a class="btn btn-primary btn-lg" href="#" role="button">
-          Learn more
-        </a>
-      </div>
+      <Navbars />
+      <Jumbo />
 
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes1.jpg"
-              width="100%"
-            />
-            <h4>상품명</h4>
-            <h4>상품설명 & 가격 </h4>
-          </div>
-          <div className="col-md-4">
-            {" "}
-            <img
-              src="https://codingapple1.github.io/shop/shoes2.jpg"
-              width="100%"
-            />
-            <h4>상품명</h4>
-            <h4>상품설명 & 가격 </h4>
-          </div>
-          <div className="col-md-4">
-            {" "}
-            <img
-              src="https://codingapple1.github.io/shop/shoes3.jpg"
-              width="100%"
-            />
-            <h4>상품명</h4>
-            <h4>상품설명 & 가격 </h4>
-          </div>
+          {shoes.map((a, i) => {
+            return <Card shoes={shoes[i]} i={i} key={i} />;
+          })}
         </div>
       </div>
+    </div>
+  );
+}
+
+function Navbars() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+function Jumbo() {
+  return (
+    <div class="jumbotron background">
+      <h1 class="display-4">20% Season Off</h1>
+      <p class="lead">
+        This is a simple hero unit, a simple jumbotron-style component for
+        calling extra attention to featured content or information.
+      </p>
+
+      <a class="btn btn-primary btn-lg" href="#" role="button">
+        Learn more
+      </a>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={`https://codingapple1.github.io/shop/shoes${props.i + 1}.jpg`}
+        width="100%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <h4>
+        {props.shoes.content} & {props.shoes.price}
+      </h4>
     </div>
   );
 }
